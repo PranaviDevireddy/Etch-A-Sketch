14cef570f42e324afe69416b71385addc0a68743
PranaviDevireddy <158205918+PranaviDevireddy@users.noreply.github.com>
2026-01-17 17:28:00 +0530

    Update e.js

--- e.js
+++ e.js
@@ -1,24 +1,8 @@
 
 let grids = document.querySelector(".container")
 
-for(let i=0;i<256;i++){
-    let div = document.createElement("div");
-    div.className = "grid";
-    grids.appendChild(div);
-}
-
-let btn = document.querySelector(".button");
-
-let size=0;
-
-btn.addEventListener("click",() => {
-    size = prompt("Enter the number of squares per side");
-        if(size > 100) {
-            alert("Enter the size below 101");
-        }
-
-        else{
-            grids.innerHTML="";
+function createGrid(size){
+    grids.innerHTML="";
             for(let i=0;i<size*size;i++){
                 let div = document.createElement("div");
                 div.className = "grid";
@@ -27,9 +11,68 @@ btn.addEventListener("click",() => {
             }
             grids.style.gridTemplateColumns = `repeat(${size},1fr)`;
 
+    addclickevents();
+}
+
+function addclickevents(){
+    const gridss = document.querySelectorAll(".grid");
+        gridss.forEach((grid)=>{
+            grid.addEventListener("click",()=>{
+                grid.style.backgroundColor = "blue";
+            });
+        })
+}
+
+createGrid(16);
+
+
+let btn = document.querySelector(".button");
+
+btn.addEventListener("click",() => {
+
+    size = prompt("Enter the number of squares per side");
+        if(size > 100) {
+            alert("Enter the size below 101");
         }
 
+        else{
+            createGrid(size);
+        }
+    });
 
-}
-);
+
+let bt = document.querySelector("#bt");
+
+bt.addEventListener("click",()=>{
+    const gridss = document.querySelectorAll(".grid");
+    gridss.forEach((grid)=>{
+        grid.style.backgroundColor=" rgb(246, 246, 246)";
+    });
+});
+
+let rm =  document.querySelector("#remove");
+
+rm.addEventListener("click",()=>{
+    const gridss = document.querySelectorAll(".grid");
+    gridss.forEach((grid)=>{
+        grid.addEventListener("click",()=>{
+            grid.style.backgroundColor=" rgb(246, 246, 246)";
+        })
+    });
+})
+
+let complete=document.querySelector("#rmcom");
+
+rmcom.addEventListener("click",()=>{
+    addclickevents();
+});
+
+let x= document.querySelector("#color");
+x.addEventListener("click",()=>{
+    let color = prompt("Enter the color");
+    grid.style.hover.backgroundColor = "${color}";
+    addclickevents();
+});
+
+
 
